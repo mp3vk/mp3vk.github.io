c19d76d3a3f5cf868a236bf4eaf6045aeac6df15
mp3vk <arsen.4e4en@gmail.com>
2020-07-31 03:41:22 +0500

    a

--- functions.js
+++ functions.js
@@ -4,11 +4,21 @@ async function get_vk_data(access_token, count = 0, offset = 0, need_user = 0) {
 		url: url,
 		dataType: "jsonp"});
 	var ret = {};
+	if (need_user) {
+		ret.name = data.response.items[0].name;
+		data.response.items.shift();
+	}
+	ret.items = []
+	data.response.items.forEach(function(item) {
+		item.thumb = (item.album && item.album.thumb) ? item.album.thumb.photo_68 : '';
+		delete item.album;
+		delete item.ads;
+		delete item.main_artists;
+		ret.items.append(item);
+	})
 	ret.items = data.response.items;
 	ret.count = data.response.count;
-	if (need_user) {
-		ret.name = ret.items[0].name;
-		ret.items.shift();
-	}
+	
+
 	return ret;
 }
