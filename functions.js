90574f7f62034ce835c40abb7b324f1ab869256f
mp3vk <arsen.4e4en@gmail.com>
2020-07-31 02:36:17 +0500

    a

--- functions.js
+++ functions.js
@@ -1,7 +1,13 @@
 async function get_vk_data(count = 0, offset = 0, need_user = 0) {
 	const access_token = document.cookie;
 	const url = 'https://api.vk.com/method/audio.get?need_user=' + need_user + '&count=' + count + '&offset=' + offset + '&v=5.90&access_token=' + access_token;
-	return await $.ajax({
+	var data = await $.ajax({
 		url: url,
 		dataType: "jsonp"});
+	data = data.response;
+	if (need_user) {
+		data.name = data.items[0].name;
+		data.items.shift();
+	}
+	return data;
 }
--- test.js
+++ test.js
@@ -1,11 +1,12 @@
+const vk_acess_token = document.cookie;
 describe("get data from vk", function() {
-
 	it("Получить все аудиозаписи с именем пользователя", async function() {
-		var data = await get_vk_data(need_user = 1);
+		var data = await get_vk_data(vk_acess_token, need_user = 1);
 		assert.typeOf(data.name, 'string');
 		assert.typeOf(data.count, 'int');
 		assert.typeOf(data.items, 'array');
 		assert.typeOf(data.asdf, 'undefined');
+		assert.isNotEmpty(data.items);
 		for (item in data.items) {
 			assert.typeOf(item.title, 'string');
 			assert.typeOf(item.artist, 'string');
@@ -17,5 +18,10 @@ describe("get data from vk", function() {
 		}
 
 	});
+	it("Получить несколько аудиозаписей", async function() {
+		var all_audios = await get_vk_data(vk_acess_token);
+		var slice_audios = await get_vk_data(vk_acess_token, count = 10, offset = 2);
+		assert.equal(slice_audios.items, all_audios.items.slice(2, 12));
+	})
 
 });
