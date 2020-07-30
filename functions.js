fc0b2bb37309f97860d30fade21727e046ab0025
mp3vk <arsen.4e4en@gmail.com>
2020-07-31 03:44:23 +0500

    a

--- functions.js
+++ functions.js
@@ -8,13 +8,13 @@ async function get_vk_data(access_token, count = 0, offset = 0, need_user = 0) {
 		ret.name = data.response.items[0].name;
 		data.response.items.shift();
 	}
-	ret.items = []
+	ret.items = [];
 	data.response.items.forEach(function(item) {
 		item.thumb = (item.album && item.album.thumb) ? item.album.thumb.photo_68 : '';
 		delete item.album;
 		delete item.ads;
 		delete item.main_artists;
-		ret.items.append(item);
+		ret.items.push(item);
 	})
 	ret.count = data.response.count;
 	
