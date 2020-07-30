c4c067af6e41ca5819c9d585a34f9fc03b56664d
mp3vk <arsen.4e4en@gmail.com>
2020-07-30 19:37:40 +0500

    a

--- test.js
+++ test.js
@@ -1,9 +1,21 @@
 describe("get data from vk", function() {
 
-  it("Получить все аудиозаписи", async function() {
-  	var data = await get_vk_data();
-  	console.log(data);
+	it("Получить все аудиозаписи с именем пользователя", async function() {
+		var data = await get_vk_data(need_user = 1);
+		assert.typeOf(data.name, 'string');
+		assert.typeOf(data.count, 'int');
+		assert.typeOf(data.items, 'array');
+		assert.typeOf(data.asdf, 'undefined');
+		for (item in data.items) {
+			assert.typeOf(item.title, 'string');
+			assert.typeOf(item.artist, 'string');
+			assert.typeOf(item.thumb, 'string');
+			assert.typeOf(item.url, 'string');
+			assert.typeOf(item.asdf, 'undefined');
+			assert.typeOf(item.duration, 'int');
+			assert.typeOf(item.date, 'int');
+		}
 
-  });
+	});
 
 });
